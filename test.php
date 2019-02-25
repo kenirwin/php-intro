5060835800703d70f610b0ee79784e277034a50e
EC2 Default User <ec2-user@ip-172-31-95-183.ec2.internal>
2019-02-25 18:03:41 +0000

    added test.php updates

--- test.php
+++ test.php
@@ -1,16 +1,44 @@
-
-<h1>Test</h1>
-<a href="index.php">Return to index</a>
-<hr />
-<pre>
-     &lt;?php
-
-         /* This is a test */
-
-         print 'yourmom';</pre>
-<hr />
 <?php
+include_once ('/var/www/inc/connect.php'); //returns $db
 
-/* This is a test */
+try { 
+    // user input is dangerous; we won't use it as-is
+    $user_title = 'dog';
+    $user_id = 1;
 
-print 'yourmom';
+    // a fairly ridiculous query with two user inputs
+    $query = "SELECT * FROM test_table WHERE title = ? OR id = ?";
+    
+    // instead of $db->query, use $db->prepare to handle user input
+    $stmt = $db->prepare($query);
+
+    // execute the prepared query with an array of one or more inputs
+    // the prepare/execute pairing will sanitize the inputs
+    // this one uses both user inputs in the order used in the query
+    $stmt->execute(array($user_title,$user_id));
+    
+    // proceed as usual
+    // use the FETCH_ASSOC mode to pair values with column names
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+
+    $lines = ''; //define an empty list of HTML table lines
+    foreach ($rows as $row) {
+        $lines .= '<tr><td>'.$row['id'].'</td><td>'.$row['title'].'</td><td>'.$row['description'].'</td></tr>'.PHP_EOL;
+    }
+
+    // now that the content is compiled, create a table and header
+    print '<table>'.PHP_EOL;
+    print '<thead><tr><td>ID</td><td>Name</td><td>Description</td></thead>'.PHP_EOL;
+    // and print the contents inside
+    print '<tbody>'.PHP_EOL;
+    print $lines;
+    print '</tbody>'.PHP_EOL;
+
+    // and close the table
+    print '</table>'.PHP_EOL;
+
+} catch (PDOException $e) {
+    print 'ERROR: '.$e->getMessage(); 
+}
+?>
