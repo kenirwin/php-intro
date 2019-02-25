d144c6cec3c14ae56187229667f949c5c74a5f8b
EC2 Default User <ec2-user@ip-172-31-95-183.ec2.internal>
2019-02-25 18:00:14 +0000

    updated create.php to handle new format

--- create.php
+++ create.php
@@ -5,8 +5,12 @@ if (array_key_exists('content', $_POST)) {
     $output.= '<title>'.$_POST['title'].'</title>'.PHP_EOL;
     $output.= '<link rel="stylesheet" type="text/css" href="styles.css">'.PHP_EOL;
     $output.= '</head>'.PHP_EOL;
+    $output.= '<body>'.PHP_EOL;
+    $output.= '<nav>'.PHP_EOL;
+    $output.= '<?php include (\'index.php\'); ?>'.PHP_EOL;
+    $output.= '</nav>'.PHP_EOL;
+    $output.= '<main>'.PHP_EOL;
     $output.= '<h1>'.$_POST['title'].'</h1>'.PHP_EOL;
-    $output.= '<a href="index.php">Return to index</a>'.PHP_EOL;
     $output.= '<hr />'.PHP_EOL;
     $output.= '<pre>'.PHP_EOL;
     $content = htmlspecialchars($_POST['content']);
@@ -15,7 +19,8 @@ if (array_key_exists('content', $_POST)) {
     $output.= '</pre>'.PHP_EOL;
     $output.= '<hr />'.PHP_EOL;
     $output.= $_POST['content'];
-    
+    $output.= '</main>'.PHP_EOL;
+
     $file = FormatFileName($_POST['title']);
     header('Content-type: text/plain');
     print $file.PHP_EOL;
