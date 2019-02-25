9ead4e820b364f152a472fa2b551d92dafbc525e
EC2 Default User <ec2-user@ip-172-31-95-183.ec2.internal>
2019-02-25 17:58:55 +0000

    added ul to index page

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 <?php
 $files = scandir('./');
 sort($files); // this does the sorting
+print '<ul>';
 foreach($files as $file){
     //    if (! preg_match('/(^\.)|(\~)/',$file)) {
     if (preg_match('/(^\d)/',$file) &! preg_match('/\~/',$file)) {
-        echo'<li><a href="./'.$file.'">'.$file.'</a></li>'.PHP_EOL;
+        $display = preg_replace ('/-/', ' ', $file);
+        echo'<li><a href="./'.$file.'">'.$display.'</a></li>'.PHP_EOL;
     }
 }
+print '</ul>';
